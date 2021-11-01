58ebf39786badaff27b9ee513eccf8a1ebd946ec
Your Name <you@example.com>
2021-11-01 08:36:37 -0400

    add github action

---
+++ .github/scripts/build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+add-apt-repository --yes ppa:ubuntu-wine/ppa
+add-apt-repository --yes ppa:arx/release
+apt-get update -d
+apt-get install -y -q innoextract wine python-software-properties
+wine --version
+innoextract --version
+"./scripts/innosetup/innoinstall.sh"
+cp scripts/innosetup/iscc /usr/local/bin/iscc
+iscc /? 2> /dev/null | grep "Inno Setup Preprocessor"
+npm install
+patch --forward --reject-file=- node_modules/gogs-client/lib/request.js < gogs-client-lib-request.diff || ( EXIT_CODE=$?; if [ $EXIT_CODE -gt 1 ]; then exit $EXIT_CODE; fi )
+npm install gulp -g
+npm install bower -g
+npm test
+wget --no-verbose "https://btt-writer-resources.s3.amazonaws.com/resource_containers.zip"
+if [ -f resource_containers.zip ]; then rm -r ./src/index; fi
+unzip -qq resource_containers.zip -d ./src/index/
+test -f src/index/index.sqlite
+test -d src/index/resource_containers
+bower install
+test -d src/components
+gulp prince
+test -d src/prince
+gulp build --win
+gulp build --linux
+gulp build --osx
+gulp release
