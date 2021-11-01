4662752844ea0d9bb6913f2f30c1b2653e5de48c
Your Name <you@example.com>
2021-11-01 08:41:20 -0400

    make exec

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -3,7 +3,7 @@
 
 # Decrypt the file
 # --batch to prevent interactive command
-# --yes to assume "yes" for questions
+# --yes to assume "yes" for questions 
 
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
 --output src/config/private.json private.json.gpg
