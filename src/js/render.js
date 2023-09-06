704ea8223d016e2cb20d8a951b237ee139a6c4a6
Maxim <murmax82@gmail.com>
2023-09-06 13:59:58 +0400

    changed parragraph regex

--- src/js/render.js
+++ src/js/render.js
@@ -227,7 +227,7 @@ function Renderer() {
         },
 
         replaceParagraphs: function (text) {
-            return text.replace(/\\p[^a-z]/g, "\n");
+            return text.replace(/\\p\W/g, "\n");
         },
 
         displayConflicts: function (content) {
