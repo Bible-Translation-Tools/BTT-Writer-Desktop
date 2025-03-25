0a33f549b8c9c877c584706718b3f175642c0584
Maxim <murmax82@gmail.com>
2025-03-25 13:07:37 +0400

    try to use macos runner

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -3,13 +3,17 @@
 set -x
 
 #sudo add-apt-repository --yes ppa:ubuntu-wine/ppa
-sudo dpkg --add-architecture i386
-sudo mkdir -pm755 /etc/apt/keyrings
-sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
-sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/jammy/winehq-jammy.sources
-sudo add-apt-repository --yes ppa:arx/release
-sudo apt-get update -d
-sudo apt-get install -y -q --install-recommends innoextract winehq-stable software-properties-common
+#sudo dpkg --add-architecture i386
+#sudo mkdir -pm755 /etc/apt/keyrings
+#sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
+#sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/jammy/winehq-jammy.sources
+#sudo add-apt-repository --yes ppa:arx/release
+#sudo apt-get update -d
+#sudo apt-get install -y -q --install-recommends innoextract winehq-stable software-properties-common
+
+brew install --cask wine-stable
+brew install innoextract
+
 wine --version
 innoextract --version
 
