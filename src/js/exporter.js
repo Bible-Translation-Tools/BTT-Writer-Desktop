03cc14eb5ddc6dbca498242d835a466b7e32dfa0
mXaln <murmax82@gmail.com>
2026-08-28 18:58:45 +0400

    fixed messages

--- src/js/exporter.js
+++ src/js/exporter.js
@@ -47,7 +47,7 @@ function ExportManager(configurator, git, reporter, translate) {
                     });
                 })
                 .catch(function (err) {
-                    throw "Error creating backup: " + err;
+                    throw "Error creating backup: " + (err && err.error && err.error.message) || err;
                 });
         },
 
